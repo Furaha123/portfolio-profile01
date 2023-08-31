d819c695602b6b518d92feda8eaee0b0683a9ab8
Furaha123 <furahamoses41@gmail.com>
2023-08-31 23:53:31 +0200

    insertions added

--- js/app.js
+++ js/app.js
@@ -121,3 +121,13 @@ navToggle.addEventListener("click", function () {
 
   links.classList.toggle("show-links");
 });
+
+document.querySelector("#show-login").addEventListener("click", function () {
+  document.querySelector(".popup").classList.add("active");
+});
+
+document
+  .querySelector(".popup .close-btn")
+  .addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+  });
